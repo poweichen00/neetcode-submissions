36adcee0e7ae68decaf2d5ed6815f441d3b99b26
CHEN PO WEI <106531929+poweichen00@users.noreply.github.com>
2026-08-26 12:18:06 +0800

    Add: last-stone-weight - submission-1

---
+++ Data Structures & Algorithms/last-stone-weight/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # 為了模擬 Max Heap，把所有石頭重量變成負數
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        # 只要還有至少兩顆石頭，就繼續撞
+        while len(stones) > 1:
+            # 取出「最大」的石頭
+            # 最小的負數代表最大的原始重量
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
+            # first - second
+            # -8 - (-7) = -1
+            # -1 代表重量 1
+            if second > first:
+                heapq.heappush(stones, first - second)
+        # 如果最後沒有石頭，補一個 0
+        # 這樣 stones[0] 就一定存在
+        stones.append(0)
+        return abs(stones[0])
